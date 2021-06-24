b72a3497b92bbac2bd32fbdfa4b069111b69b983
Valtteri Huttunen <vvahuttunen@protonmail.com>
2021-06-24 10:30:20 +0300

    fix: virheelliset hakemistopolut

--- django/django_test/urls.py
+++ django/django_test/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from tutorial.quickstart import views
+from django_test.django_rest import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -27,6 +27,6 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
