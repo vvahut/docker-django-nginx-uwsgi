203e515469b7c91f46819e07482d2dcc1fd57b04
Valtteri Huttunen <vvahuttunen@protonmail.com>
2021-06-29 09:20:31 +0300

    run.sh skripti konteille

---
+++ run.sh
@@ -0,0 +1,5 @@
+sudo docker rm -f nginx_test
+sudo docker rm -f django_test
+sudo docker network create net1
+sudo docker run -d --net=net1 --name django_test django_test
+sudo docker run -d --net=net1 --name nginx_test -p 80:80 --volumes-from django_test nginx_test
