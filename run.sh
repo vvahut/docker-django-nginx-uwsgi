2634fe70717c747f49e07de35356087d309e54fd
Valtteri Huttunen <vvahuttunen@protonmail.com>
2021-06-29 10:08:55 +0300

    delete run.sh -- ei tarvita, koska docker-compose

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo docker rm -f nginx_test
-sudo docker rm -f django_test
-sudo docker network create net1
-sudo docker run -d --net=net1 --name django_test django_test
-sudo docker run -d --net=net1 --name nginx_test -p 80:80 --volumes-from django_test nginx_test
