2ec50953945cbc75f2ebf35d7066073966178b3c
Valtteri Huttunen <vvahuttunen@protonmail.com>
2021-06-30 09:15:49 +0300

    rest-framwork -> /api

--- django/django_test/urls.py
+++ django/django_test/urls.py
@@ -26,7 +26,7 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
