49f8f0651c55cc1e0bbe6ad69586a0f652a0cf6a
Valtteri Huttunen <vvahuttunen@protonmail.com>
2021-07-02 07:55:20 +0300

    added api-testi -> nextjs

--- next.js/pages/index.js
+++ next.js/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-function Home() {
+function Home({content}) {
   return (
     <>
       <Link href="/about">
@@ -9,8 +9,33 @@ function Home() {
       </Link>
       <h1>Home</h1>
       <img src="/assets/home.png" />
-    </>
+      <ul>
+        <li>{content.users}</li>
+      </ul>
+      </>
   )
+}
+
+export async function getServerSideProps({req}) {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const headers = {
+    "Accept": "application/json",
+    //"client_id": "1001125",
+    //"client_secret": "876JHG76UKFJYGVHf867rFUTFGHCJ8JHV"
+  }
+
+  const res = await fetch('http://stack-nginx/api', { headers: headers });
+  //const res = await fetch('http://192.168.32.4/api', { headers: headers });
+  const content = await res.json()
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      content,
+    },
+  }
 }
 
 export default Home
