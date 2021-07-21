7961c3e4646abeb273af575b3c3a3420a276be7d
Valtteri Huttunen <vvahuttunen@protonmail.com>
2021-07-21 15:19:05 +0300

    moved manage commands to entryfile

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #cp -r /home/django_test/static/. /home/django_test/static_volume
+python3.8 manage.py migrate
+python3.8 manage.py collectstatic
 exec /usr/local/bin/uwsgi --ini "/home/django_test/uwsgi.ini"
