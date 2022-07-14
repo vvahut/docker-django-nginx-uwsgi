3eac1e6558f2d96392844e1227af1b12dad671e1
Valtteri Huttunen <vvahuttunen@protonmail.com>
2022-07-14 19:21:15 +0300

    Update entrypoint

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #cp -r /home/django_test/static/. /home/django_test/static_volume
 python3.8 manage.py migrate
-python3.8 manage.py collectstatic
+python3.8 manage.py collectstatic --noinput
+python3.8 manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL --username $DJANGO_SUPERUSER_USERNAME
 exec /usr/local/bin/uwsgi --ini "/home/django_test/uwsgi.ini"
