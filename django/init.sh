371bbb3bf495cd4ebe66884e5826d36a53a322fb
Valtteri Huttunen <vvahuttunen@protonmail.com>
2022-07-18 12:24:21 +0300

    Fix: remove superuser creation from init script

--- django/init.sh
+++ django/init.sh
@@ -3,4 +3,3 @@
 set -x
 python3.8 manage.py migrate
 python3.8 manage.py collectstatic --noinput
-python3.8 manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL --username $DJANGO_SUPERUSER_USERNAME
